882b3a363f56398b16804b6b7eb646baaa47d9f0
ChooChooRayk <choochoorayk@gmail.com>
2025-07-08 11:53:44 +0200

    call button update plot

--- scenes/bessel_scipy_data_plotter.gd
+++ scenes/bessel_scipy_data_plotter.gd
@@ -3,12 +3,12 @@ extends Control
 class_name BesselPlotterFromTxtData
 
 enum BESSEL_TYPE {
-    J,
-    Y,
-    Jp,
-    Yp,
-    H1,
-    H2,
+	J,
+	Y,
+	Jp,
+	Yp,
+	H1,
+	H2,
 }
 
 @export var path_to_dir := ""
@@ -21,20 +21,20 @@ var path_to_data_format := "data_{type}{idx}.txt"
 var path_to_data :String
 
 func make_path_to_data()->void:
-    var type_str : String = ""
-    match bessel_type:
-        BESSEL_TYPE.J:
-            type_str = "j"
-        BESSEL_TYPE.Y:
-            type_str = "y"
-        BESSEL_TYPE.H1:
-            type_str = "h1"
-        BESSEL_TYPE.H2:
-            type_str = "h2"
-        BESSEL_TYPE.Jp:
-            type_str = "jp"
-        BESSEL_TYPE.Yp:
-            type_str = "yp"
-    # ---
-    path_to_data = path_to_dir+path_to_data_format.format({"type":type_str, "idx":bessel_idx})
-    function_line._path_to_line_data_points = path_to_data
+	var type_str : String = ""
+	match bessel_type:
+		BESSEL_TYPE.J:
+			type_str = "j"
+		BESSEL_TYPE.Y:
+			type_str = "y"
+		BESSEL_TYPE.H1:
+			type_str = "h1"
+		BESSEL_TYPE.H2:
+			type_str = "h2"
+		BESSEL_TYPE.Jp:
+			type_str = "jp"
+		BESSEL_TYPE.Yp:
+			type_str = "yp"
+	# ---
+	path_to_data = path_to_dir+path_to_data_format.format({"type":type_str, "idx":bessel_idx})
+	function_line._path_to_line_data_points = path_to_data
