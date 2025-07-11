1c685946de59c12966494cff9fb8f09e2f182ea5
ChooChooRayk <choochoorayk@gmail.com>
2025-07-11 16:20:09 +0200

    space to tab

--- scenes/math_shader_display.gd
+++ scenes/math_shader_display.gd
@@ -23,36 +23,36 @@ extends Control
 
 
 func _ready() -> void:
-    update = true
+	update = true
 
 func _process(delta: float) -> void:
-    if update:
-        update_shader_params()
-        # ---
-        bessel_plotter_from_txt_data.bessel_idx  = bessel_idx
-        bessel_plotter_from_txt_data.bessel_type = bessel_type
-        function.set_shader_parameter("_n", bessel_idx)
-        # ---
-        bessel_plotter_from_txt_data.make_path_to_data()
-        function_line.set_plot_frame(x_min, x_max,y_min, y_max)
-        function_line.extract_data()
-        function_line.set_line()
-        update = false
-    pass
-    
+	if update:
+		update_shader_params()
+		# ---
+		bessel_plotter_from_txt_data.bessel_idx  = bessel_idx
+		bessel_plotter_from_txt_data.bessel_type = bessel_type
+		function.set_shader_parameter("_n", bessel_idx)
+		# ---
+		bessel_plotter_from_txt_data.make_path_to_data()
+		function_line.set_plot_frame(x_min, x_max,y_min, y_max)
+		function_line.extract_data()
+		function_line.set_line()
+		update = false
+	pass
+	
 func update_shader_params()->void:
-    var x_amp := (x_max-x_min)
-    var y_amp := (y_max-y_min)
-    var origin_pos = -Vector2((x_max+x_min)/2., -(y_max+y_min)/2.)
-    # ---
-    grid.set_shader_parameter("_scale_axes", Vector2(x_amp, y_amp))
-    grid.set_shader_parameter("grid_origin", origin_pos)
-    # ---
-    origin.set_shader_parameter("_scale_axes", Vector2(x_amp, y_amp))
-    origin.set_shader_parameter("grid_origin", origin_pos)
-    origin.set_shader_parameter("grid_period", Vector2(x_amp, y_amp))
-    # ---
-    function.set_shader_parameter("_scale_axes", Vector2(x_amp, y_amp))
-    function.set_shader_parameter("grid_origin", origin_pos)
-    function.set_shader_parameter("which_bessel_func", int(bessel_type))
-    return
+	var x_amp := (x_max-x_min)
+	var y_amp := (y_max-y_min)
+	var origin_pos = -Vector2((x_max+x_min)/2., -(y_max+y_min)/2.)
+	# ---
+	grid.set_shader_parameter("_scale_axes", Vector2(x_amp, y_amp))
+	grid.set_shader_parameter("grid_origin", origin_pos)
+	# ---
+	origin.set_shader_parameter("_scale_axes", Vector2(x_amp, y_amp))
+	origin.set_shader_parameter("grid_origin", origin_pos)
+	origin.set_shader_parameter("grid_period", Vector2(x_amp, y_amp))
+	# ---
+	function.set_shader_parameter("_scale_axes", Vector2(x_amp, y_amp))
+	function.set_shader_parameter("grid_origin", origin_pos)
+	function.set_shader_parameter("which_bessel_func", int(bessel_type))
+	return
